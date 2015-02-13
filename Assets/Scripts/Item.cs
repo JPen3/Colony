42f2ff02e8738f1dacd3a18f1b42f2d2c4dd9c87
Jerry Pendleton III <jerry_pendleton@yahoo.com>
2015-02-13 16:36:09 -0600

    Items/Icons
    Added test icons and started on Item script.

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+	public string itemName;
+	public int itemID;
+	public string itemDesc;
+	public Texture2D itemIcon;
+	public ItemType itemType;
+
+	public enum ItemType {
+		WEAPON,
+		RESOURCE,
+		TOOL,
+		FORTIFICATION
+	}
+}
