c94058c7f0cf316a7485cc0ce6be0fb52b6e7edb
blochowcizr <blochowiczr@yahoo.com>
2015-02-23 16:39:57 -0600

    AllTheStuff!!!!!!!!!!!!!!
    added a room selector scene that works!!!!!!

---
+++ Assets/Scripts/CameraLerpScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLerpScript : MonoBehaviour {
+
+    public GameObject targetPoint;
+    public GameObject MainLerpPoint;
+    public bool isLerpin = false;
+    public float LerpTimer = 0.0f;
+    public float LerpsUpTime = 1.5f; 
+    public float speed = 3.0F;
+    private float startTime;
+    private float journeyLength;
+    public float smooth = 3.0F;
+    
+    // Use this for initialization
+	void Start () {
+        targetPoint = MainLerpPoint;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(isLerpin)
+        {
+            SelectRoom(targetPoint);
+            LerpTimer += Time.deltaTime; 
+            if(LerpTimer >= LerpsUpTime)
+            {
+                endLerp();
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            targetPoint = MainLerpPoint;
+            isLerpin = true;
+            LerpTimer = 0.0f; 
+        }
+	}
+
+    public void SelectRoom(GameObject RoomCenter)
+    {
+        PositionLerp(RoomCenter);
+    }
+
+    public void PositionLerp(GameObject NewTarget)
+    {
+        transform.position = Vector3.Lerp(transform.position, NewTarget.transform.position, smooth*Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, NewTarget.transform.rotation, Time.deltaTime * speed);
+    }
+    public void endLerp()
+    {
+        isLerpin = false; 
+    }
+}
---
+++ Assets/Scripts/OnRoomSelectScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnRoomSelectScript : MonoBehaviour {
+
+    public GameObject MainCamera;
+    public GameObject LerpPoint;
+    
+    // Use this for initialization
+	void Start () {
+        if (GameObject.Find("Main Camera"))
+        {
+            MainCamera = GameObject.Find("Main Camera");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        MainCamera.GetComponent<CameraLerpScript>().targetPoint = LerpPoint;
+        MainCamera.GetComponent<CameraLerpScript>().isLerpin = true;
+        MainCamera.GetComponent<CameraLerpScript>().LerpTimer = 0.0f; 
+    }
+}
