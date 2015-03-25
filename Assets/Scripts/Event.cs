355691ce9e8e544177eae772c3a7c8c89443a6cb
Jerry Pendleton III <jerry_pendleton@yahoo.com>
2015-03-25 16:41:11 -0500

    I'M BATMAN!

---
+++ Assets/Scripts/Event.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Event{
+	public string eventName;
+	public int eventID;
+	public string eventDesc;
+	
+	public Event(string name, int id, string desc){
+		eventName = name;
+		eventID = id;
+		eventDesc = desc;
+	}
+	
+	public Event(){
+		
+	}
+}
