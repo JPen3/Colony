d2a6556b070b34897a301f9270f34ab60ffa25ca
blochowcizr <blochowiczr@yahoo.com>
2015-03-31 04:24:54 -0500

    New Prototype Inventory
    i've started experimenting with how to apply our 3D assets onto our UI.
    I have a visually working prototype. You can hover over each icon and it
    will rotate! This is in a deactivated panel inside the canvas.

---
+++ Assets/Scripts/InventoryRotationScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryRotationScript : MonoBehaviour {
+
+    public bool onHover = false;
+    public Quaternion old_rot;
+    public GameObject IconObj; 
+
+    // Use this for initialization
+	void Start () {
+        old_rot = IconObj.transform.rotation; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(onHover)
+        {
+            IconObj.transform.eulerAngles += new Vector3(0, 30 * Time.deltaTime, 0); 
+        }
+	}
+
+    void OnMouseOver()
+    {
+        onHover = true; 
+    }
+
+    void OnMouseExit()
+    {
+        onHover = false;
+        IconObj.transform.rotation = old_rot; 
+    }
+}
