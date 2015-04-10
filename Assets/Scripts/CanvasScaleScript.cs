b3ceb87218c2658d6c2f5f19da5f5eec0eb12d05
blochowcizr <blochowiczr@yahoo.com>
2015-04-09 22:51:31 -0500

    UI Screen Scaling!!!!!!!!
    now the UI canvas will scale to any screen resolution while it is in the
    worldspace!!!! this is one step closer to implementing our 3d
    Icons!!!!!!!

---
+++ Assets/Scripts/CanvasScaleScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class CanvasScaleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Screen Width: " + Screen.width + " Screen Height: " + Screen.height); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width, Screen.height);
+        this.GetComponent<RectTransform>().anchoredPosition3D = new Vector3((float)0, (float)0, (float)Screen.height / 235); 
+	}
+}
